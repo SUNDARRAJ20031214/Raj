88dc15fe2b564163a22401035a12cf30024237d8
SUNDARRAJ20031214 <msundarajbe2025@gmail.com>
2025-11-15 07:37:58 +0530

    Update Index.py

--- Index.py
+++ Index.py
@@ -89,21 +89,22 @@ if uploaded_file is not None:
             st.write(f"**R² Score:** {r2:.2f}")
 
         # Prediction
-        
- st.subheader("🧪 Predict on your New Data")
+    st.subheader("🧪 Predict on your New Data")
 
- user_input = {}
- for col in X.columns:
-    val = st.text_input(f"Enter value for {col}")
-    if val:
-     try:
+    user_input = {}
+    for col in X.columns:
+      val = st.text_input(f"Enter value for {col}")
+      if val:
+      try:
         user_input[col] = float(val)
-     except:
+      except:
         user_input[col] = val
 
- if st.button("Predict"):
-   user_df = pd.DataFrame([user_input])
+    if st.button("Predict"):
+      user_df = pd.DataFrame([user_input])
             user_df = user_df.reindex(columns=X.columns, fill_value=0)
             prediction = model.predict(user_df)[0]
             st.success(f"🔮 Prediction: {prediction}")
-                     
+
+
+ 
